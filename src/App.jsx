3c87ed546ab5694eb8863752a0439a63935726ce
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-05-08 14:12:03 +0200

    [UPD] Initialize header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
+import Header from './components/Header'
+import Main from './components/Main'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Header />
+      <Main />
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <div className="container">
+                <h1>
+                    Learn Web development
+                </h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header
