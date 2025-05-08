4221394918e236a6981e61b457eac24a9c78e19f
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-05-08 14:32:46 +0200

    [UPD] Initialize buttons section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { useState } from 'react'
+import languages from '../assets/languages'
 
 const Main = () => {
     return (
-        <div>
-
-        </div>
+        <main>
+            <div className="container">
+                <div id="btn-section">
+                    <h2>Languages</h2>
+                    {languages.map((language) => {
+                        return (
+                            <button key={language.id} className="btn" id={language.id}>
+                                {language.title}
+                            </button>
+                        )
+                    })}
+                </div>
+                <div id="card-section"></div>
+            </div>
+        </main>
     )
 }
 
